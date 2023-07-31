b80509f9d7c3211f2949d0f90b1717f711b18a32
phil395 <philiplgvn@gmail.com>
2023-08-01 00:43:37 +0300

    add useAutosize

---
+++ src/hooks/useAutosize.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+import autosize from "autosize"
+
+export const useAutosize = (deps: unknown) => {
+  const ref = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) return
+    autosize(element)
+    return () => {
+      autosize.destroy(element)
+    }
+  }, [])
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) return
+    autosize.update(element)
+  }, [deps])
+
+  return ref
+}
