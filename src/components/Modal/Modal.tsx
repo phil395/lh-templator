73cab2d880117df1d65ac6755f6e68e67e377c0d
phil395 <philiplgvn@gmail.com>
2023-08-01 00:42:27 +0300

    add Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,28 @@
+import { useRef, type FC, type PropsWithChildren } from "react";
+import { useBodyClass, useClickOutside, useKeyPress } from "../../hooks";
+import { Button } from "../Button";
+import styles from "./Modal.module.css";
+
+interface Props {
+  hide: () => void;
+}
+
+export const Modal: FC<PropsWithChildren<Props>> = ({ hide, children }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  useKeyPress(ref, hide, "Escape")
+  useClickOutside(ref, hide)
+  useBodyClass("overflow-hidden")
+
+  return (
+    <div className={styles.overlay}>
+      <div>
+        <div className={styles.container} ref={ref} tabIndex={1}>
+          <Button icon="close" color="red" className={styles.close} onClick={hide} />
+          <div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
