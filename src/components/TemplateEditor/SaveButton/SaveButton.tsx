7da99cdb6af0a919aac7cb03918f0b2428871359
phil395 <philiplgvn@gmail.com>
2023-08-01 00:52:41 +0300

    add SaveButton

---
+++ src/components/TemplateEditor/SaveButton/SaveButton.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+import { useTemplateEditorStore } from "../../../store";
+import { Button } from "../../Button";
+
+export const SaveButton: FC = () => {
+  const save = useTemplateEditorStore(({ save }) => save)
+
+  return (
+    <Button
+      onClick={save}
+      content="Save"
+      icon="done"
+      color="green"
+    />
+  )
+}
