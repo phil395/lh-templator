7f0fae0dfb0d2603cc3d49f8b52898c707418158
phil395 <philiplgvn@gmail.com>
2023-08-01 00:55:14 +0300

    add VariablesAssigner

---
+++ src/components/Preview/VariablesAssigner/VariablesAssigner.tsx
@@ -0,0 +1,51 @@
+import { type FC } from "react"
+import { usePreviewStore } from "../../../store"
+import styles from "./VariablesAssigner.module.css"
+import { useDebouncedCallback } from "../../../hooks"
+
+interface ItemVariableProps {
+  variableName: string
+  updateVariableValue: (variableName: string, newValue: string) => void
+}
+
+const DELAY = 75
+
+const ItemVariable: FC<ItemVariableProps> = ({ variableName, updateVariableValue }) => {
+  const update = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateVariableValue(variableName, e.target.value)
+  }
+
+  const debouncedUpdate = useDebouncedCallback(update, DELAY)
+
+  return (
+    <li className={styles.item}>
+      <p>{"{"}{variableName}{"}"}</p>
+      <input
+        className="text-field"
+        onChange={debouncedUpdate}
+      />
+    </li>
+  )
+}
+
+interface VariablesAssignerProps {
+  arrVarNames: string[]
+}
+
+export const VariablesAssigner: FC<VariablesAssignerProps> = ({ arrVarNames }) => {
+  const updateVariableValue = usePreviewStore(({ updateVariableValue }) => updateVariableValue)
+
+  if (!arrVarNames) return null
+
+  return (
+    <ul>
+      {arrVarNames.map((variableName) => (
+        <ItemVariable
+          key={variableName}
+          variableName={variableName}
+          updateVariableValue={updateVariableValue}
+        />
+      ))}
+    </ul>
+  )
+}
---
+++ src/components/Preview/VariablesAssigner/index.ts
@@ -0,0 +1,1 @@
+export { VariablesAssigner } from './VariablesAssigner'
