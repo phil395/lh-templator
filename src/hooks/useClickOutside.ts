a495ab831a56efa825a0299dddd7c8568d827c43
phil395 <philiplgvn@gmail.com>
2023-08-01 00:44:13 +0300

    add useClickOutside

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+type EventHandler = (() => void) | ((event: Event) => void)
+
+export const useClickOutside = (
+  ref: React.RefObject<Element>,
+  handler: EventHandler
+): void => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (
+        !ref.current ||
+        ref.current.contains(event.target as Node | null)
+      ) {
+        return;
+      }
+      handler(event)
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [handler, ref]);
+}
