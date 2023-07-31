c5f0c5c261d4c6b12ffc60a6794be6b06905b953
phil395 <philiplgvn@gmail.com>
2023-08-01 00:56:45 +0300

    refactoring App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { FC } from "react";
+import type { FC } from "react"
+import { Transition, TemplateEditor, OpenTemplateEditorButton } from "./components"
+import { ModalProvider } from "./context";
+import { DEFAULT_VAR_NAMES, TemplateNode } from "./models";
+import { useTemplateEditorStore } from "./store";
+import styles from './App.module.css'
 
 export const App: FC = () => {
+  const open = useTemplateEditorStore(({ open }) => open)
+
+  const arrVarNames = localStorage.arrVarNames
+    ? JSON.parse(localStorage.arrVarNames)
+    : DEFAULT_VAR_NAMES;
+
+  const template = localStorage.template
+    ? JSON.parse(localStorage.template)
+    : null
+
+  const callbackSave = async (template: TemplateNode[]) => {
+    localStorage.setItem("template", JSON.stringify(template))
+  }
 
   return (
-    <main>
-
-    </main>
+    <>
+      <Transition>
+        {open && (
+          <ModalProvider>
+            <TemplateEditor
+              arrVarNames={arrVarNames}
+              template={template}
+              callbackSave={callbackSave}
+            />
+          </ModalProvider>
+        )}
+      </Transition>
+      <Transition enter={styles['delayed-transition-opacity']}>
+        {!open && (
+          <OpenTemplateEditorButton />
+        )}
+      </Transition>
+    </>
   );
 }
