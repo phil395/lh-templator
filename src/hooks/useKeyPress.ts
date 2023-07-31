b3439b89fed7295edd7ed950483bb0d1b8575634
phil395 <philiplgvn@gmail.com>
2023-08-01 00:44:55 +0300

    add useKeyPress

---
+++ src/hooks/useKeyPress.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+type EventHandler = (() => void) | ((event: Event) => void)
+
+export const useKeyPress = (
+  ref: React.RefObject<Element>,
+  handler: EventHandler,
+  key: string
+): void => {
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (
+        !ref.current ||
+        event.key !== key
+      ) {
+        return;
+      }
+      handler(event)
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [handler, key, ref]);
+}
